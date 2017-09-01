288ad55e04192b90ec01eace28b7e9be92ce7de7
lukas.steiger@siemens.com <lukas.steiger@hotmail.com>
2017-09-01 17:31:28 +0200

    clean up unneccary code

--- resources/ui/HistoryEntry.js
+++ resources/ui/HistoryEntry.js
@@ -9,17 +9,14 @@ define([
     "dijit/_WidgetsInTemplateMixin",
     "dijit/Tooltip",
     "dojo/dom-construct",
-    "dojo/query",
-    "dojo/text!./templates/HistoryEntry.html",
-    "dojo/ready",
-], function(declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, Tooltip, domConstruct, query, template, ready) {
-	return declare("com.siemens.bt.jazz.rtc.workItemEditor.presentation.statusHistory.ui.HistoryEntry", [ _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin ], {
+    "dojo/text!./templates/HistoryEntry.html"
+], function(declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, Tooltip, domConstruct, template) {
+	return declare("com.siemens.bt.jazz.rtc.workItemEditor.presentation.statusHistory.ui.HistoryEntry", [ _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
         
         templateString: null,
         stateData: null,
 		
         constructor: function(stateData) {
-            this.inherited(arguments);
             this.templateString = template;
             this.stateData = stateData;
         },
