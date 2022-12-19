7684d01f7956a2ca9e8bad276a4ed90b6c277fe7
dave007700 <dave007700@gmail.com>
2022-12-19 08:54:01 +0100

    Update Webpack for Actions-Build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,66 @@
 const webpack = require('webpack');
 const JazzUpdateSitePlugin = require('jazz-update-site-webpack-plugin');
 const packageJson = require('./package.json');
+const core = require('@actions/core');
+const DisableOutputWebpackPlugin = require("disable-output-webpack-plugin");
+const RemovePlugin = require("remove-files-webpack-plugin");
 
 module.exports = (env) => {
-    env && env.buildUUID && console.info(`Build UUID is passed along: '${env.buildUUID}'`);
-    const version = env && env.buildUUID || packageJson.version;
+	env && env.buildUUID && console.info(`Build UUID is passed along: '${env.buildUUID}'`);
+	const version = env && env.buildUUID || packageJson.version;
+	const projectId = "com.siemens.bt.jazz.rtc.workitemeditor.presentation.statushistory";
 
-    const config = {
-        entry: {
-            StatusHistory: './index.js' // not used, prevent webpack from failing
-        },
-        output: {
-            filename: '[name]Bundle.js' // not used, prevent webpack from failing
-        },
-        plugins: [
-            new JazzUpdateSitePlugin({
-                appType: 'ccm',
-                projectId: "com.siemens.bt.jazz.rtc.workitemeditor.presentation.statushistory",
-                acceptGlobPattern: [
-                    'resources/**',
-                    'META-INF/**',
-                    'plugin.xml',
-                ],
-                projectInfo: {
-                    author: packageJson.author,
-                    copyright: packageJson.author,
-                    description: packageJson.description,
-                    license: packageJson.license,
-                    version: version,
-                },
-            }),
-        ],
-    };
-    return config;
+	const config = {
+		entry: {
+			StatusHistory: './index.js' // not used, prevent webpack from failing
+		},
+		output: {
+			filename: '[name]Bundle.js' // not used, prevent webpack from failing
+		},
+		plugins: [
+			new JazzUpdateSitePlugin({
+				appType: 'ccm',
+				projectId: projectId,
+				acceptGlobPattern: [
+					'resources/**',
+					'META-INF/**',
+					'plugin.xml',
+				],
+				projectInfo: {
+					author: packageJson.author,
+					copyright: packageJson.author,
+					description: packageJson.description,
+					license: packageJson.license,
+					version: version,
+				},
+			}),
+			new RemovePlugin({
+				before: {
+					root: __dirname,
+					test: [
+						{
+							folder: "./",
+							method: filePath => {
+								return new RegExp(
+									/com\.siemens\.bt\.jazz\.rtc\.workitemeditor\.presentation\.statushistory.*\.zip$/,
+									"i"
+								).test(filePath);
+							}
+						}
+					]
+				},
+				after: {
+					root: __dirname,
+					include: ["dist"]
+				}
+			}),
+		],
+	};
+
+	if (process.env["GITHUB_ACTIONS"]) {
+		// Set the output file name for use in GitHub Actions
+		core.setOutput("output_file", `${projectId}_${version}.zip`);
+	}
+
+	return config;
 };
